2d6ed15c5ae0b03b8f3ce1dc3195c7471eb2a0ee
fullert4114 <50212315+fullert4114@users.noreply.github.com>
2019-06-10 10:54:40 -0700

    Update projectActionPage.c

--- projectActionPage.c
+++ projectActionPage.c
@@ -1,10 +1,7 @@
+
 #include <stdio.h>
 
-//int walks, int seeds, int naps, int fish, int hights, int mailmen, int run, int scream
-
-
-int main (int argc, char* argv[]) {
-
+int main (int argc, char* argv[]){ 
     
     if (argc < 5)
     {
@@ -15,34 +12,31 @@ int main (int argc, char* argv[]) {
     int seeds;
     int found2 = sscanf(argv[1], "%d", &seeds);
     if (found2 != 1){
-    	printf("First arg is not an int \n");
-    	return 1;
+        printf("First arg is not an int \n");
+        return 1;
     }
     
     
     int mailmen;
     int found6 = sscanf(argv[2], "%d", &mailmen);
     if (found6 != 1){
-    	printf("Second arg is not an int \n");
-    	return 1;
+        printf("Second arg is not an int \n");
+        return 1;
     }
     
     int run;
     int found7 = sscanf(argv[3], "%d", &run);
     if (found7 != 1){
-    	printf("Third arg is not an int \n");
-    	return 1;
-    }
+        printf("Third arg is not an int \n");
+        return 1;
+          }
     
       int scream;
     int found8 = sscanf(argv[4], "%d", &scream);
     if (found8 != 1){
-    	printf("Fourth arg is not an int \n");
-    	return 1;
+        printf("Fourth arg is not an int \n");
     }
 
-char Animal (int seeds, int mailmen, int run, int scream)
-{    
     printf("seeds: %d mailmen: %d run: %d scream: %d \n",seeds, mailmen, run, scream);
     
 {
@@ -74,6 +68,17 @@ char Animal (int seeds, int mailmen, int run, int scream)
     } else if (seeds == 1 && mailmen == 1 && run == 0 && scream ==0){
         printf("You are a Raccon!\n");
     } else if (seeds == 1 && mailmen == 1 && run == 0 && scream ==1){
+    } else if (seeds == 1 && mailmen == 0 && run == 0 && scream ==0){
+        printf("You are a Chipmunk!\n");
+    } else if (seeds == 1 && mailmen == 0 && run == 0 && scream ==1){
+        printf("You are Tasha!\n");
+    } else if (seeds == 1 && mailmen == 0 && run == 1 && scream ==0){
+        printf("You are a Chicken!\n");
+    } else if (seeds == 1 && mailmen == 0 && run == 1 && scream ==1){
+        printf("You are a Friendly Seagull!\n");
+    } else if (seeds == 1 && mailmen == 1 && run == 0 && scream ==0){
+        printf("You are a Raccon!\n");
+    } else if (seeds == 1 && mailmen == 1 && run == 0 && scream ==1){
         printf("You are a Seagull!\n");
     } else if (seeds == 1 && mailmen == 1 && run == 1 && scream ==0){
         printf("You are a Quiet Gaurd Goose!\n");
@@ -81,7 +86,13 @@ char Animal (int seeds, int mailmen, int run, int scream)
         printf("You're a Goose!\n");
     } else{
         printf("Should never run"); 
-   }
+   } 
+int mathRequirement;  
+{
+  int Math1 = (seeds + mailmen + run + scream);
+  int math = (3.14159265358979323846 * Math1 * Math1);
+  return math;
+} 
 }
 }
-}
+
